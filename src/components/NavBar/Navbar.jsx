d8dbf7a920a653a01ba2c8fd95cb4b86e544cd22
overkill489 <overkill.alt.489@gmail.com>
2026-08-11 12:04:17 -0500

    08/10/2026 @12:04

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,24 +1,69 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faSackXmark } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
-const Navbar = () => {
-  const [search, setSearch] = useState("");
-  const navigate = useNavigate();
+const Nav = () => {
+  function openMenu() {
+    document.body.classList += " menu--open";
+  }
 
-
+  function closeMenu() {
+    document.body.classList.remove("menu--open");
+  }
 
   return (
-    <nav className="navbar">
-      <h1 className="navbar__title">Movie</h1>
-
-      <div className="navbar__links">
-        <a href="#popular">Popular</a>
-        <a href="#top_rated">Top rated</a>
-        <a href="#upcoming">Upcoming</a>
+    <nav>
+      <div className="nav__container">
+        <Link to="/" className="nav-title">
+          Movies
+        </Link>
+        <ul className="nav__links">
+          <li className="nav__list">
+            <Link to="/" className="nav__link">
+              Home
+            </Link>
+          </li>
+          <li className="nav__list">
+            <Link to="Popular" className="nav__link">
+              Popular
+            </Link>
+          </li>
+          <li className="nav__list">
+            <Link to="Popular" className="nav__link">
+              Top Rated
+            </Link>
+          </li>
+          <button className="btn__menu" onClick={openMenu}>
+            <FontAwesomeIcon icon={faBars} />{" "}
+          </button>
+        </ul>
+        <div className="menu__backdrop">
+          <button className="btn__menu btn__menu--close" onClick={closeMenu}>
+            <FontAwesomeIcon icon={faSackXmark} />
+          </button>
+          <ul className="menu__links">
+            <li className="menu__list">
+              <Link to="/" className="menu__link">
+                Home
+              </Link>
+            </li>
+            <li className="menu__list">
+              <Link to="/" className="menu__link">
+                Popular
+              </Link>
+            </li>
+            <li className="menu__list">
+              <Link to="/" className="menu__link">
+                Top Rated
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
 };
 
-export default Navbar;
+export default Nav;
